ce6e37cca60985f5f13308cc3bb1257627b3bd1f
Shinmera <shinmera@tymoon.eu>
2015-05-26 19:41:51 +0200

    Fixed fibonacci heap impl.

--- fibonacci-heap.lisp
+++ fibonacci-heap.lisp
@@ -32,20 +32,25 @@
 
 (defmethod print-object ((cell fibonacci-heap-cell) stream)
   (print-unreadable-object (cell stream :type T)
-    (format NIL "~s" (value cell))))
+    (format stream "~s" (value cell))))
+
+(defmethod insert ((new-heap fibonacci-heap-cell) (heap fibonacci-heap))
+  (setf (parent new-heap) heap)
+  (setf (child heap)
+        (if (child heap)
+            (merge-heaps (child heap) new-heap)
+            new-heap)))
 
 (defmethod insert (value (heap fibonacci-heap))
-  (let ((new-heap (make-instance 'fibonacci-heap-cell :value value :parent heap)))
-    (setf (child heap)
-          (if (child heap)
-              (merge-heaps (child heap) new-heap)
-              new-heap))))
+  (insert (make-instance 'fibonacci-heap-cell :value value) heap))
 
 (defmethod merge-heaps ((a fibonacci-heap-cell) (b fibonacci-heap-cell))
-  (setf (left a) b
-        (right b) a
-        (right (left a)) (right b)
-        (left (right b)) (left a))
+  (let ((ra (right a))
+        (lb (left b)))
+    (setf (right a) b
+          (left b) a
+          (left ra) lb
+          (right lb) ra))
   (if (< (value a) (value b))
       a
       b))
@@ -55,12 +60,14 @@
        (value (child heap))))
 
 (defun map-fibonacci-children (heap function)
-  (let* ((end (left (child heap))))
-    (loop for current = (child heap) then next
-          for next = (right current)
-          do (funcall function current)
-          until (eql end current))
-    heap))
+  (when (child heap)
+    (let* ((end (left (child heap))))
+      (mapcar function
+              ;; Save it, for it might be modified.
+              (loop for current = (child heap) then (right current)
+                    collect current
+                    until (eql end current)))
+      heap)))
 
 (defmacro do-fibonacci-children ((current heap) &body body)
   `(block NIL (map-fibonacci-children ,heap (lambda (,current) ,@body))))
@@ -74,8 +81,15 @@
         (when found (return found))))))
 
 (defun remove-child-directly (child)
-  (setf (right (left child)) (right child)
-        (left (right child)) (left child))
+  (let ((left (left child))
+        (right (right child)))
+    (when (eql (child (parent child)) child)
+      (setf (child (parent child))
+            (unless (eql left child) left)))
+    (setf (left right) left
+          (right left) right)
+    (setf (left child) child
+          (right child) child))
   child)
 
 (defun fuse-fibonacci-heap (heap)
@@ -104,10 +118,11 @@
                     (setf (parent larger) smaller)
                     ;; Update metadata
                     (setf (marker smaller) NIL)
-                    (incf (rank smaller))))
+                    (incf (rank smaller))
+                    (setf (aref rank (rank smaller)) smaller)))
              (if (< (value current) (value ranked))
-                 (order ranked current)
-                 (order current ranked)))))))
+                 (order current ranked)
+                 (order ranked current)))))))
     (setf (child heap) minimum))
   heap)
 
@@ -124,50 +139,74 @@
   (let ((child (child heap)))
     (when child
       (cond ((eql child (right child))
+             (when (child child)
+               (setf (parent (child child)) heap))
              (setf (child heap) (child child)))
             ((not (child child))
-             (remove-child-directly child)
-             ;; ??? This seems like an oversight in the
-             ;; algorithm. Inserting and removing directly
-             ;; will result in O(n) for delete-minimum...
-             (setf (child heap) (find-minimum-neighbor (left child))))
+             (let ((neighbor (left child)))
+               (remove-child-directly child)
+               (setf (child heap) neighbor)))
             (T
-             (merge-heaps (right child) (child child))
-             (setf (child heap) (right child))
-             (fuse-fibonacci-heap heap)))
-      (value child))))
+             (let ((right (right child)))
+               (remove-child-directly child)
+               (merge-heaps right (child child))
+               (setf (child heap) right))))
+      (fuse-fibonacci-heap heap)
+      (values (value child)
+              child))))
 
 (defmethod decrease-key ((cell fibonacci-heap-cell) amount)
-  (setf (marker cell) T)
+  (setf (marker cell) T
+        (value cell) amount)
   (let ((to-insert ())
         (root cell))
-    (loop until (not (typep root 'fibonacci-heap-cell))
+    (loop while (typep root 'fibonacci-heap-cell)
           do (cond ;; Already marked! Unmark, remove, and note for insertion.
-                   ((marker root)
-                    (setf (marker root) NIL)
-                    (remove-child-directly root)
-                    (push root to-insert))
-                   ;; Unmarked, we can stop now.
-                   (T
-                    (setf (marker root) T)
-                    ;; Ok, we're done, ascend the rest
-                    (loop until (not (typep root 'fibonacci-heap-cell))
-                          do (setf root (parent root)))
-                    (return)))
+               ((marker root)
+                (setf (marker root) NIL)
+                (remove-child-directly root)
+                (push root to-insert))
+               ;; Unmarked, we can stop now.
+               (T
+                (setf (marker root) T)
+                ;; Ok, we're done, ascend the rest
+                (loop while (typep root 'fibonacci-heap-cell)
+                      do (setf root (parent root)))
+                (return)))
              (setf root (parent root)))
-    ;; Merge in and relink minimum.
-    (let ((minimum (child root)))
-      (dolist (current to-insert)
-        (when (< (value root) (value minimum))
-          (setf minimum root))
-        (merge-heaps current (child root))
-        (setf (parent current) root))
-      (setf (child root) minimum))
+    ;; Relink back top. This will automatically ensure proper parents and minimum.
+    (dolist (current to-insert)
+      (insert current root))
     (values cell root)))
 
 (defmethod delete-cell ((cell fibonacci-heap-cell))
   (let ((value (value cell)))
-    (decrease-key cell most-positive-fixnum)
+    (decrease-key cell most-negative-fixnum)
     ;; We must have arrived at the root.
     (delete-minimum (parent cell))
     value))
+
+(defmethod label ((cell fibonacci-heap-cell))
+  (value cell))
+
+(defun render-fibonacci-heap (heap output)
+  (let ((temp (merge-pathnames "fibonacci-tree-render.dot" (uiop:temporary-directory))))
+    (with-open-file (stream temp :direction :output :if-exists :supersede)
+      (format stream "digraph fib {~%")
+      (labels ((render-cell (parent)
+                 (format stream "N_~a -> N_~a [style=\"bold\"];"
+                         (if (typep parent 'fibonacci-heap-cell) (value parent) "ROOT")
+                         (value (child parent)))
+                 (do-fibonacci-children (node parent)
+                   (let ((id (value node)))
+                     (format stream "N_~a [label=\"~a\"]" id (label node))
+                     (format stream "N_~a -> N_~a;" id (value (left node)))
+                     (format stream "N_~a -> N_~a;" id (value (right node)))
+                     (when (child node)
+                       (render-cell node))))))
+        (render-cell heap))
+      (format stream "~&}"))
+    (uiop:run-program (format NIL "dot -Tsvg \"~a\" > \"~a\""
+                              (uiop:native-namestring temp)
+                              (uiop:native-namestring output)))
+    output))
